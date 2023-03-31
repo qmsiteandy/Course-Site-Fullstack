e2030dfa6bf62ded40331f384bcf3af82c140dcb
ht93084 <ht93084@gmail.com>
2023-03-31 16:58:07 +0800

    Feat: 教師新增課程

--- routes/courseRouter.js
+++ routes/courseRouter.js
@@ -3,13 +3,32 @@ const passport = require("passport");
 const router = express.Router();
 const mysql = require("../config/mysqlConnection");
 
-// 選取全部課程
-router.get("/", (req, res, next) => {
-  mysql.query("SELECT * FROM course", (err, result) => {
-    if (err) next(err);
-    else return res.status(200).send(result);
+router
+  .route("/")
+  // 選取全部課程
+  .get((req, res, next) => {
+    mysql.query("SELECT * FROM course", (err, result) => {
+      if (err) next(err);
+      else return res.status(200).send(result);
+    });
+  })
+  // 發布新課程
+  .post(passport.authenticate("jwt", { session: false }), (req, res, next) => {
+    // 確認權限是否為 admin 或 teacher
+    if (req.user.permission < 1) return res.status(403).send("權限不足");
+
+    const { name, desc, price } = req.body;
+
+    // DB 操作
+    mysql.query(
+      "INSERT INTO course (course_teacherId, course_name, course_desc, course_price) VALUE(?,?,?,?)",
+      [req.user.userId, name, desc, price],
+      (err, result) => {
+        if (err) next(err);
+        else return res.status(201).send("新增課程成功");
+      }
+    );
   });
-});
 
 // // 從關鍵字尋找
 // router.get("/search", (req, res, next) => {
