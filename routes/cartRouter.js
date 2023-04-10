b24c82c7d5f6c3d6c75f203d95d9ebffc94704a4
ht93084 <ht93084@gmail.com>
2023-04-10 17:21:11 +0800

    Feat: 購物車頁面顯示所有已加入商品功能

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -2,6 +2,33 @@ const express = require("express");
 const router = express.Router();
 const passport = require("../config/passport");
 const Cart = require("../models/cartModel");
+const mysql = require("../config/mysqlConnection");
+
+// 取得購物車所有課程資訊
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res, next) => {
+    // 從 MongoDB 中找尋對應使用者的購物車項目
+    let { courseId_array } = await Cart.findOne({
+      studentId: req.user.id,
+    });
+
+    // 取出對應課程資料
+    mysql.query(
+      "SELECT course.id, course.name as name, course.description, course.price, user.name as teacherName \
+      FROM course LEFT JOIN user ON course.teacherId = user.id  \
+      WHERE course.id IN (?)",
+      [courseId_array],
+      (err, result) => {
+        if (err) {
+          next(err);
+        }
+        return res.status(200).send(result);
+      }
+    );
+  }
+);
 
 // 新增購物車內容
 router.post(
