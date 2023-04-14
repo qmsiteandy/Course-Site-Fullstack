b1704a11af698b90785014b2a2d5c56d7dd7eb34
ht93084 <ht93084@gmail.com>
2023-04-14 15:16:38 +0800

    Feat: 設定 page router 的 JWT 認證機制、重新引導至登入頁的警示訊息

--- routes/pageRouter.js
+++ routes/pageRouter.js
@@ -17,20 +17,51 @@ router.get("/course/:id", (req, res) => {
   res.render("coursePage.ejs");
 });
 
-router.get("/mycourse", (req, res) => {
-  res.render("mycourse.ejs");
-});
+// JWT token 失效訊息
+const token_fail_msg = "登入權證失效，請重新登入";
 
-router.get("/backend", (req, res) => {
-  res.render("backend.ejs");
-});
+router.get(
+  "/mycourse",
+  passport.authenticate("jwt", {
+    session: false,
+    failureRedirect: `/login?msg=${token_fail_msg}`,
+  }),
+  (req, res) => {
+    res.render("mycourse.ejs");
+  }
+);
 
-router.get("/teacher_register", (req, res) => {
-  res.render("teacherRegister.ejs");
-});
+router.get(
+  "/backend",
+  passport.authenticate("jwt", {
+    session: false,
+    failureRedirect: `/login?msg=${token_fail_msg}`,
+  }),
+  (req, res) => {
+    res.render("backend.ejs");
+  }
+);
 
-router.get("/shopcart", (req, res) => {
-  res.render("shopcart.ejs");
-});
+router.get(
+  "/teacher_register",
+  passport.authenticate("jwt", {
+    session: false,
+    failureRedirect: `/login?msg=${token_fail_msg}`,
+  }),
+  (req, res) => {
+    res.render("teacherRegister.ejs");
+  }
+);
+
+router.get(
+  "/shopcart",
+  passport.authenticate("jwt", {
+    session: false,
+    failureRedirect: `/login?msg=${token_fail_msg}`,
+  }),
+  (req, res) => {
+    res.render("shopcart.ejs");
+  }
+);
 
 module.exports = router;
